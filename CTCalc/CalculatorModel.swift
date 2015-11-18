a2ecaea622a3214221616a98a04888a884bf5424
Theodore Abshire <TheodoreAbshire@Gmail.com>
2015-11-18 14:52:36 -0800

    Ok I made invert work.

--- CTCalc/CalculatorModel.swift
+++ CTCalc/CalculatorModel.swift
@@ -196,8 +196,13 @@ class CalculatorModel
 					tokens.removeRange(numStart!...i)
 					
 					//and add the number
+					var mult:Int = 1
+					if number.characters.first == "⁻"
+					{
+						number = number.substringFromIndex(number.startIndex.advancedBy(1))
+					}
 					guard let value = Double(number) else { throw CalculatorError.WhyDoINeedMultipleCases("BAD SYNTAX") }
-					tokens.insert(Token(symbol: "num", order: kOrderLiteral, effect0: { value }), atIndex: numStart!)
+					tokens.insert(Token(symbol: "num", order: kOrderLiteral, effect0: { value * mult }), atIndex: numStart!)
 					
 					//and start over
 					return try collapseTokens(tokens)
