8c9f1fb0308cab02d0f576896747df7226b77a82
Notsosof1 <cawhitlatch2@gmail.com>
2015-11-19 09:45:38 -0800

    Thursday11-19-15

--- ButtonMakerViewController.swift
+++ ButtonMakerViewController.swift
@@ -11,37 +11,45 @@ import Parse
 
 class ButtonMakerViewController: UIViewController, UITextViewDelegate {
     
-    var pressed = false
+    var blueWhitebutton : UIImageView?
     
-    @IBOutlet weak var buttonView: UIView!
-    @IBOutlet weak var customButtonName: UITextField!
+    @IBOutlet weak var buttonView: UIImageView!
     
-    
-    @IBAction func redButtonPressed(sender: AnyObject) {
-        
+    @IBAction func blueWhiteButtonPressed(sender: UIButton) {
+        let blueWhiteButton = UIImage(named: "custombutton6")
+        buttonView.image = blueWhiteButton
+    }
+   
+    @IBAction func yellowOrangeButton(sender: AnyObject) {
+        let blueWhiteButton = UIImage(named: "custombutton4")
+        buttonView.image = blueWhiteButton
     }
     
     @IBAction func orangeButtonPressed(sender: AnyObject) {
+        let blueWhiteButton = UIImage(named: "custombutton2")
+        buttonView.image = blueWhiteButton
     }
-    
-    
-    @IBAction func yellowButtonPressed(sender: AnyObject) {
-    }
-    
     
     @IBAction func greenButtonPressed(sender: AnyObject) {
+        let blueWhiteButton = UIImage(named: "custombutton3")
+        buttonView.image = blueWhiteButton
     }
     
-    
-    @IBAction func purpleButtonPressed(sender: AnyObject) {
+    @IBAction func blueButtonPressed(sender: AnyObject) {
+        let blueWhiteButton = UIImage(named: "custombutton1")
+        buttonView.image = blueWhiteButton
     }
     
+    @IBAction func grayButtonPressed(sender: AnyObject) {
+        let blueWhiteButton = UIImage(named: "custombutton5")
+        buttonView.image = blueWhiteButton
+    }
     
 
     
-    //           SAVES BUTTONS TO PARSE
-    class func uploadButton(image: UIImage, imageName: String, completion: (success: Bool) -> ()) {
-		
+            //           SAVES BUTTONS TO PARSE
+    func uploadButton(image: UIImage, imageName: String, completion: (success: Bool) -> ()) {
+		//   Select the background color you want.  That changes the color of the UIView.   Add up to 5 letters of text/symbols to describe the custom button function.  This will be the "symbol (name)" of the button.
 		//to add this to the buttons list
 		//let dcvc = navigationController!.parentViewController as! DraggableContainerViewController
 		//dcvc.addToken(token:Token)
@@ -69,14 +77,22 @@ class ButtonMakerViewController: UIViewController, UITextViewDelegate {
     }
     
     @IBAction func saveCustomButton(sender: AnyObject) {
-        
-        
+        uploadButton(buttonView.image!, imageName: "image") { (success) -> () in
+            if success {
+                print("yay")
+            }else {
+                print("boo")
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         let loadedNib = NSBundle.mainBundle().loadNibNamed("CalculatorButton", owner: self, options: nil)[0] as! ButtonCollectionViewCell
+        
+        
 //        loadedNib.frame = CGRect(x: 0, y: 0, width: cell.bounds.width, height: cell.bounds.height)
 //        loadedNib.label.text = readOnlyButtons[path.row].symbol
 //        self.addSubview(self.view)
