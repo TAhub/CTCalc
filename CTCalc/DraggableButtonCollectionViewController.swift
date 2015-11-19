98b72f5f9ac11f1db3a727b468bd7c57417fe62a
Theodore Abshire <TheodoreAbshire@Gmail.com>
2015-11-19 10:56:39 -0800

    Added a nice white margin.

--- CTCalc/DraggableButtonCollectionViewController.swift
+++ CTCalc/DraggableButtonCollectionViewController.swift
@@ -120,6 +120,9 @@ class DraggableButtonCollectionViewController: UICollectionViewController, Dragg
         
 	}
 	
+	//to make sure the edit mode animation appears
+	private var firstEditMode = true
+	
 	override func viewWillAppear(animated: Bool)
 	{
 		super.viewWillAppear(animated)
@@ -133,6 +136,11 @@ class DraggableButtonCollectionViewController: UICollectionViewController, Dragg
 		}
 		
 		(self.parentViewController as! DraggableContainerViewController).dragDelegate = self
+		
+		if !editMode
+		{
+			firstEditMode = false
+		}
 	}
 	
 	override func viewDidAppear(animated: Bool) {
@@ -247,14 +255,21 @@ class DraggableButtonCollectionViewController: UICollectionViewController, Dragg
 					let startX = collectionView!.contentOffset.x + cell.frame.origin.x
 					let startY = collectionView!.contentOffset.y + cell.frame.origin.y
 					
+					//load the container edges
+					let containerMargin:CGFloat = 6
+					let containerView = UIView(frame: CGRect(x: startX - containerMargin, y: startY - containerMargin, width: cell.bounds.width + 2 * containerMargin, height: cell.bounds.height + 2 * containerMargin))
+					containerView.layer.cornerRadius = 10
+					containerView.backgroundColor = UIColor.whiteColor()
+					
 					//load the cell from a nib
 					let loadedNib = NSBundle.mainBundle().loadNibNamed("CalculatorButton", owner: self, options: nil)[0] as! ButtonCollectionViewCell
-					loadedNib.frame = CGRect(x: startX, y: startY, width: cell.bounds.width, height: cell.bounds.height)
+					loadedNib.frame = CGRect(x: containerMargin, y: containerMargin, width: cell.bounds.width, height: cell.bounds.height)
 					loadedNib.token = readOnlyButtons[path.row]
+					containerView.addSubview(loadedNib)
 					
-					view.addSubview(loadedNib)
+					view.addSubview(containerView)
 					
-					pickedUp = PickedUpCell(cellRow: path.row, appearance: loadedNib, viewControllerFrom: self)
+					pickedUp = PickedUpCell(cellRow: path.row, appearance: containerView, viewControllerFrom: self)
 				}
 			case UIGestureRecognizerState.Changed:
 				if let pickedUp = pickedUp
