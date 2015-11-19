1d158187545cb1529a680375adeeb51fd3be16d9
Cisco <franciscoraglandjr@gmail.com>
2015-11-19 11:43:20 -0800

    Added tutorial pageVC

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -16,7 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        //Parse stuff
         setUpParse()
+        
+        //PageViewController Stuff
+        var pageController = UIPageControl.appearance()
+        pageController.pageIndicatorTintColor = UIColor.lightGrayColor()
+        pageController.currentPageIndicatorTintColor = UIColor.blackColor()
+        pageController.backgroundColor = UIColor.whiteColor()
+        
         return true
     }
     
